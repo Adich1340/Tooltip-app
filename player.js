9d1546804f75b6e256e8975b4a7ea9f9f76bd0f8
Adich1340 <adich1340@gmail.com>
2020-10-15 18:24:09 +0300

    Fourth commit - last version

--- player.js
+++ player.js
@@ -1,83 +1,43 @@
 var OUTER_DIV = '<div  class="sttip"><div class="tooltip in"><div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner"></div></div></div>'
-    
-function waitForInitialScriptLoad() {
-        const src = "https://code.jquery.com/jquery-3.5.1.min.js";
-        const script = document.createElement("script");
-        const styleCss = document.createElement('link'); 
-        styleCss.setAttribute('href', 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css');
-        styleCss.setAttribute('rel', 'stylesheet');
-        styleCss.setAttribute('type', 'text/css');
 
-        script.src = src;
-        document.head.appendChild(script);
-        document.head.appendChild(styleCss);
-        
-
-        return new Promise((res) => {
-          script.addEventListener("load", () => {
-            window.$ = window.jQuery;
-            res(true);
-          });
-        });
-}
-
-(async function () {
-    
-    await waitForInitialScriptLoad();
-    const $ = window.$;
-
-    function jsScript() {
-
-      $(document).ready(function(){
-
-      var tool = function(param) {
+  var tool = function(param) {
           let steps = param.data.structure.steps;
           let tiplates = param.data.tiplates;
           let style = param.data.css;
-
-          //filtering actions obj only
+          
+//           filter actions only
           let actions = steps.map(element => element.action);
 
-          //filtering tips type obj only
+//           filter tips only
           let tips = actions.filter((action) => action.type === 'tip');
 
           addTip(actions, tips, tiplates, style, tipNumber = 0);
 
       }
-  
-    $.getScript("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=tool&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867");
-
-    })
-
-        
+    
+         
        function addTip(actions, tips, tiplates, style, tipNumber){
                 
-                //parsing tip, hover and style from text to html
-                var outerHTNL = $.parseHTML(OUTER_DIV);
-                var tipHTML = $.parseHTML(tiplates.tip);
-                var hoverTipHTML = $.parseHTML(tiplates.hoverTip);
+//                parsing tip, hover and style from text to array
+                var outerDiv = $(OUTER_DIV);
+                var tipDiv = $.parseHTML(tiplates.tip);
+                var hoverTipDiv = $.parseHTML(tiplates.hoverTip);
                 
-                //add div tip and hoverTip
-                $(tips[tipNumber].selector).after(outerHTNL);
-                $(".popover-inner").append(tipHTML)
-                $( ".popover-inner" ).hover(
-                      function() {
-                        $( this ).append(hoverTipHTML);
-                      }, function() {
-                            $( this ).find(".ir-hoverTip").remove();
-                          }
-                        );
+//                 add tip css
+                $("head").append('<style>'+style+'</style>');
+                
+//                 add div tip 
+                $(tips[tipNumber].selector).after(outerDiv);
 
-                //add tip css
-                $('<style>'+style+'</style>').appendTo('.popover-inner');
-
-                //add tooltip's #step and content 
+//                 add tipDiv array to html
+                $(".popover-inner").append(tipDiv)
+              
+//                 add tooltip's #step and content 
                 $(".popover-content > div").html(tips[tipNumber].contents['#content'])
                 $(".steps-count > span").html(tipNumber)
                 $(".steps-count span:last-child").html(tips.length - 1)
 
-
-                //tooltip "x" click and tooltip "next" click
+//                 tooltip "x" onclick and tooltip "next" onclick
                 $(".popover-title button").click(function() {
                     $(".sttip").remove();
                 })
@@ -87,16 +47,51 @@ function waitForInitialScriptLoad() {
                        ++tipNumber < 4 ? addTip(actions, tips, tiplates, style, tipNumber) : console.log("FINISH TO LOAD");                
                 })                    
         }
+
+
+function waitForInitialScriptLoad() {
+        const src = "https://code.jquery.com/jquery-3.5.1.min.js";
+        const script = document.createElement("script");
+        const styleCss = document.createElement('link'); 
+        const bootstrapStyle = document.createElement('link');
+      
+//         set attributes to styleCss
+        styleCss.setAttribute('href', 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css');
+        styleCss.setAttribute('rel', 'stylesheet');
+        styleCss.setAttribute('type', 'text/css');
+
+        script.src = src;
+        document.head.appendChild(script);
+        document.head.appendChild(styleCss);
+
+        return new Promise((res) => {
+          script.addEventListener("load", () => {
+            window.$ = window.jQuery;
+            res(true);
+          });
+        });
+}
+
+
+(async function () {
+    
+    await waitForInitialScriptLoad();
+    const $ = window.jQuery;
+
+    function jsScript() {
+        $(document).ready(function(){  
+            $.getScript("https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=tool&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867");
+
+        })
     }
 
-    function inject(fn, ) {
+    function inject(fn) {
       const script = document.createElement('script')
       script.text = `(${fn.toString()})();`
       document.documentElement.appendChild(script)
     }
   
     inject(jsScript);
-
   })();
 
 
